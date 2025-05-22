cf6beb83703414e4e21aeed5fec48204e7848f0c
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-05-22 11:53:14 -0400

    Create add-to-balance.ts

---
+++ indexer/src/contracts/jb-multi-terminal/add-to-balance.ts
@@ -0,0 +1,25 @@
+import { type Context, type Event, ponder } from "ponder:registry";
+import { project } from "ponder:schema";
+
+ponder.on("JBMultiTerminal:AddToBalance", addToBalance);
+
+async function addToBalance(params: {
+  event: Event<"JBMultiTerminal:AddToBalance">;
+  context: Context<"JBMultiTerminal:AddToBalance">;
+}) {
+  const { context, event } = params;
+  const { args } = event;
+  const { id: chainId } = context.chain;
+
+  const { projectId: _projectId, amount } = args;
+  const projectId = Number(_projectId);
+
+  await context.db
+    .update(project, {
+      chainId,
+      projectId,
+    })
+    .set((p) => ({
+      balance: p.balance + amount,
+    }));
+}
