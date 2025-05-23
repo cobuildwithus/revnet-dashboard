a8b39ac724704a9ee42e55f18b34c8269fe60799
riderway <riderway@runbox.com>
2025-05-23 22:29:29 +0200

    UI v0 (#1)
    Co-authored-by: Wojciech Kocjan <wojciech@kocjan.net>

---
+++ web/src/lib/wagmi-config.ts
@@ -0,0 +1,48 @@
+import { arbitrum, base, mainnet, optimism } from "viem/chains";
+import { createConfig, fallback, http } from "wagmi";
+import { coinbaseWallet, safe, walletConnect } from "wagmi/connectors";
+
+const safeConnector = safe({
+  allowedDomains: [/^app\.safe\.global$/],
+  debug: true,
+  shimDisconnect: true,
+});
+
+export const wagmiConfig = createConfig({
+  chains: [mainnet, optimism, arbitrum, base],
+  connectors: [
+    safeConnector,
+    coinbaseWallet({
+      appName: "REVNET",
+      appLogoUrl: "https://app.revnet.eth.sucks/assets/img/small-bw.svg",
+    }),
+    walletConnect({
+      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+      showQrModal: false,
+      metadata: {
+        name: "REVNET",
+        description: "Tokenize revenues and fundraises. 100% autonomous.",
+        url: "https://app.revnet.eth.sucks",
+        icons: ["https://app.revnet.eth.sucks/assets/img/small-bw.svg"],
+      },
+    }),
+  ],
+  transports: {
+    [mainnet.id]: fallback([
+      http(`https://eth-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`),
+      http(`https://mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`),
+    ]),
+    [optimism.id]: fallback([
+      http(`https://opt-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`),
+      http(`https://optimism-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`),
+    ]),
+    [base.id]: fallback([
+      http(`https://base-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`),
+      http(`https://base-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`),
+    ]),
+    [arbitrum.id]: fallback([
+      http(`https://arb-mainnet.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`),
+      http(`https://arbitrum-mainnet.infura.io/v3/${process.env.NEXT_PUBLIC_INFURA_ID}`),
+    ]),
+  },
+});
