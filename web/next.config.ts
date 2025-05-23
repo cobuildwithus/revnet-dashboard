81a118e25d8b7bd320b2ef38a924bc9715b727b1
Wojciech Kocjan <wojciech@kocjan.net>
2025-05-23 21:39:59 +0200

    UI v0

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["www.revnet.app"],
+  },
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 };
 
 export default nextConfig;
