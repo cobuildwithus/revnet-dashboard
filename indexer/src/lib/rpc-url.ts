459c793a28fc51cc05f9d82adff0e2539862f808
rocketman <20303031+rocketman-21@users.noreply.github.com>
2025-05-28 16:35:12 -0400

    use infura

--- indexer/src/lib/rpc-url.ts
+++ indexer/src/lib/rpc-url.ts
@@ -5,16 +5,16 @@ const getPrefix = (chain: "base" | "eth" | "arbitrum" | "optimism") => {
     case "base":
       return "base-mainnet";
     case "eth":
-      return "eth-mainnet";
+      return "mainnet";
     case "arbitrum":
-      return "arb-mainnet";
+      return "arbitrum-mainnet";
     case "optimism":
-      return "opt-mainnet";
+      return "optimism-mainnet";
   }
 };
 
 const constructUrl = (prefix: string) =>
-  http(`https://${prefix}.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`);
+  http(`https://${prefix}.infura.io/v3/${process.env.INFURA_API_KEY}`);
 
 export const rpcUrl = (chain: "base" | "eth" | "arbitrum" | "optimism") => {
   const prefix = getPrefix(chain);
